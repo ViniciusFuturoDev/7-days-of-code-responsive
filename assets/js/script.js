4bf7d0c35cd58cfd2fcba2d2754c05068d261e8f
ViniciusFuturoDev <136001757+ViniciusFuturoDev@users.noreply.github.com>
2023-12-19 20:33:23 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 function buttonMore() {
     const buttons = document.querySelectorAll(".button-more")
     buttons.forEach((button) => {
-        console.log(button)
         button.classList.toggle("active")
     })
 }
